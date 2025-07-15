d170b41ca24318b4a18f930c0a9d994d7110761c
Magdalena Uçar <magdalena.ucar@posteo.de>
2025-07-15 13:57:33 +0200

    Schema Erweiterung hinzugefuegt

--- models/todos.js
+++ models/todos.js
@@ -5,6 +5,7 @@ const schema = new mongoose.Schema({
     todoName: String,
     prio: String,
     datum: String
+    // hier das Schema erweitern um user_id: String
 });
 
 module.exports = mongoose.model('Todos', schema);      //'todos' ist Name der Datenbank Collection
